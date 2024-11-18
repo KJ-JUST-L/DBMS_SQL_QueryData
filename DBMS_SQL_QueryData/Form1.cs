9a05c04b2862f229bbd2c8bf98695ac70744c31e
Just L <blacklistzaza@gmail.com>
2024-11-18 15:11:16 +0700

    Add project files.

---
+++ DBMS_SQL_QueryData/Form1.cs
@@ -0,0 +1,41 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace DBMS_SQL_QueryData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+
+        public void connect()
+        {
+            string server = @".\SQLEXPRESS";
+            string db = "Minimart";
+            string strConn = string.Format(@"Data Source={0};Initial Catalog={1};Integrated Security=True ;Encrypt=False", server, db);
+            conn = new SqlConnection(strConn);
+            conn.Open();
+        }
+
+        public void disconnect()
+        {
+            conn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connect();
+            string sql = "SELECT * FROM Products";
+            da = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
